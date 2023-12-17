961b5becec0228af5e955825d1ec09d6ad75b63d
ShogoMark <trigmark87@gmail.com>
2023-12-17 10:57:00 +0100

    form.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import './App.css';
+import './form.css';
 
   function App() {
     const [formData, setFormData] = useState({
       surName: '',
       firstName: '',
       otherName:'',
+      phoneNumber: '',
       email:'',
       organization:'',
       category:'',
@@ -31,57 +32,80 @@ import './App.css';
   
     return (
       <>
+        <div
+        style={{
+        backgroundImage: `url(${process.env.PUBLIC_URL}/form-bg.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor:'rgba(255, 255, 255, 1.0)',
+      }}
+      >
+        <div class="top-reg">
         <h3>Registration Form</h3>
+        </div>
         <form class="form-floating" onSubmit={handleSubmit}>
           <div class='container'>
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingInput" placeholder="Surname" value={formData.surName}
+              <input type="text" class="form-control" id="surName" placeholder="Surname" value={formData.surName}
               onChange={handleChange}/>
-              <label for="floatingInput">Surname</label>
+              <label for="surName">Surname</label>
             </div>
             <div class="form-floating">
-              <input type="text" class="form-control" id="floatingPassword" placeholder="First name" value={formData.firstName} onChange={handleChange}/>
-              <label for="floatingPassword">First name</label>
+              <input type="text" class="form-control" id="firstName" placeholder="First name" value={formData.firstName} onChange={handleChange}/>
+              <label for="firstName">First name</label>
             </div>
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingInput" placeholder="Middle Name" value={formData.otherName}
+              <input type="text" class="form-control" id="otherName" placeholder="Middle Name" value={formData.otherName}
               onChange={handleChange}/>
-              <label for="floatingInput">Middle name</label>
+              <label for="otherName">Middle name</label>
             </div>
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" value={formData.email}
+              <input type="text" class="form-control" id="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber}
               onChange={handleChange}/>
-              <label for="floatingInput">Email</label>
+              <label for="phoneNumber">Phone Number</label>
             </div>
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="floatingInput" placeholder="Organization" value={formData.surName}
+              <input type="email" class="form-control" id="email" placeholder="name@example.com" value={formData.email}
               onChange={handleChange}/>
-              <label for="floatingInput">Organization</label>
+              <label for="email">Email</label>
             </div>
             <div class="form-floating mb-3">
-              <select class="form-select" aria-label="Default select example" value={formData.category} onChange={handleChange}>
-                <option selected>select category</option>
+              <input type="text" class="form-control" id="organization" placeholder="Organization" value={formData.organization}
+              onChange={handleChange}/>
+              <label for="organization">Organization</label>
+            </div>
+            <div class="form-floating mb-3">
+              <select class="form-select-sm" aria-label="Default select example" onChange={handleChange}>
+                <option selected>Select category</option>
                 <option value="1">Govt. Official</option>
                 <option value="2">Private Enterprise</option>
                 <option value="3">Business Owner</option>
                 <option value="4">Lecturer</option>
                 <option value="5">Student</option>
               </select>
-              <select class="form-select" value={formData.interest} onChange={handleChange}>
-                <option selected>select interest</option>
+              <select class="form-select-sm" onChange={handleChange}>
+                <option selected>Select interest</option>
                 <option value="1">Digital Inclusivity</option>
                 <option value="2">Research & Innovation Journal</option>
                 <option value="3">Remote Job Portal</option>
                 <option value="4">E-library, Coding & Robotic Center</option>
               </select>
-              <p>Do you need a certificate</p>
-              <select>
-                <option value="1">Yes</option>
-                <option value="2">No</option>
-              </select>
+              <p>Do you need a certificate
+                <select class="form-select-sm" onChange={handleChange}> 
+                  <option value="1">Yes</option>
+                  <option value="2">No</option>
+                </select>
+              </p>
             </div>
+            <button>Register</button>
           </div>
-        </form>  
+        </form>
+      </div>  
       </>
     );
   }
